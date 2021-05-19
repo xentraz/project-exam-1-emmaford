193d476c8b5e55524593d815fe9ca4ed447f8f49
Emma <69841693+xentraz@users.noreply.github.com>
2021-05-19 03:02:15 +0200

    details update

--- js/details.js
+++ js/details.js
@@ -3,6 +3,9 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
+document.querySelector('.post_loading').innerHTML = `<img src="https://cdn.dribbble.com/users/1747793/screenshots/4328938/web-loop.gif"/>`
+
+// POST DETAIL INFO
 async function getPosts(id){
   try{
     const response = await fetch('https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/posts/' + id);
@@ -14,45 +17,37 @@ async function getPosts(id){
 
     document.querySelector('.post_info').innerHTML += 
     `
-    <div class="gameDetails">
-      <div class="gameName">
-        <h2>${postsResults.slug}</h2>
-        </div>
+    <div class="post_name">
+      <h1>${postsResults.slug}</h1>
+    </div>
+    `;
+
+    document.querySelector('.post_text').innerHTML += 
+    `
+    <div class="text_container">
+    ${postsResults.content.rendered}
     </div>
     `;
 
   } catch (error) {
-    // document.querySelector('.alert').innerHTML = showAlertToUser (
-    //   'An error occured',
-    //   'danger'
-    // );
+    document.querySelector('.alert').innerHTML = showAlertToUser (
+      'An error occured',
+      'danger black_p'
+    );
     console.log(error);
   } finally {
-    // setTimeout(function () {
-    //   document.querySelector('.alert').innerHTML = ``;
-    // }, 5000)
+    setTimeout(function () {
+      document.querySelector('.alert').innerHTML = ``;
+    }, 5000)
   }
+  document.querySelector('.post_loading').innerHTML = '';
 }
-
 getPosts(id);
 
 
-// const postsCards = (featuredArray) => {
-// 	const mainElm = document.querySelector('.featured_container');
-//   for (let i = 0; i < featuredArray.length; i++) {
-//     mainElm.innerHTML += `
-//     <div class="card__containerBlog">
-//       <img class="featuredImg" src="${featuredArray[i].jetpack_featured_media_url}"/>
-//     </a>
-//       <h3 class="cardTitleBlog">${featuredArray[i].title.rendered}</h3>
-//       <div class="info-button"><a href="/html/details.html?id=${featuredArray[i].id}">More Info</a></div>
-//       </div>`;
-//   }
-// }
+// POST DETAIL IMAGES 
 
-// IMAGES 
-
-let blogAPI2 = [];
+let mediaAPI = [];
 
 const getMedia = async () => {
 	try {
@@ -60,35 +55,39 @@ const getMedia = async () => {
 			'https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/media?parent=' +id);
 
 		const mediaResponse = await response.json();
-		blogAPI2 = mediaResponse;
-    console.log(blogAPI2)
+		mediaAPI = mediaResponse;
+    console.log(mediaAPI)
 
-    mediaCards(blogAPI2);
+    mediaCards(mediaAPI);
 
     
 
 	} catch (error) {
-		// document.querySelector('.alert').innerHTML = showAlertToUser (
-    //   'An error occured',
-    //   'danger'
-    // );
+		document.querySelector('.alert').innerHTML = showAlertToUser (
+      'An error occured',
+      'danger black_p'
+    );
 		console.log(error);
 	} finally {
-		// setTimeout(function () {
-    //   document.querySelector('.alert').innerHTML = ``;
-    // }, 5000)
+		setTimeout(function () {
+      document.querySelector('.alert').innerHTML = ``;
+    }, 5000)
 	}
+  document.querySelector('.post_loading').innerHTML = '';
 };
-
 getMedia(id);
 
 
-const mediaCards = (featuredArray2) => {
-	const mainElm2 = document.querySelector('.post_images');
-  for (let i = 0; i < featuredArray2.length; i++) {
-    mainElm2.innerHTML += `
-    <div class="card__containerBlog2">
-      <img class="featuredImg" src="${featuredArray2[i].guid.rendered}"/>
+const mediaCards = (mediaArray) => {
+	const mediaElm = document.querySelector('.post_media');
+  for (let i = 0; i < mediaArray.length; i++) {
+    mediaElm.innerHTML += `
+    <div class="media_cards">
+      <img class="media_img" src="${mediaArray[i].guid.rendered}"/>
+      <div class="media_text">
+      ${mediaArray[i].caption.rendered}   
+      </div>
+    </div>
       `;
   }
 };
--- js/script.js
+++ js/script.js
@@ -42,15 +42,18 @@ const getFeatured = async () => {
     featuredCards(blogAPI);
 
 	} catch (error) {
-		// document.querySelector('.alert').innerHTML = showAlertToUser (
-    //   'An error occured',
-    //   'danger'
-    // );
+		document.querySelector('.alert').innerHTML = showAlertToUser (
+      'Error loading content',
+      'danger black_p'
+    );
 		console.log(error);
+    if (error === true) {
+      document.querySelector('.featured_container').innerHTML += ``;
+    }
 	} finally {
-		// setTimeout(function () {
-    //   document.querySelector('.alert').innerHTML = ``;
-    // }, 5000)
+		setTimeout(function () {
+      document.querySelector('.alert').innerHTML = ``;
+    }, 10000)
 	}
 };
 
@@ -64,117 +67,12 @@ const featuredCards = (featuredArray) => {
       mainElm.innerHTML += `
       <div class="featured_card">
         <img class="featured_img" src="${featuredArray[i].jetpack_featured_media_url}"/>
-      <div class="feature_card_info">
-      <h2 class="featured_card_title">${featuredArray[i].title.rendered}</h2>
-      <p class="black_p">${featuredArray[i].date}</p>
-      </div>
-      <div class="more_button"><a href="/html/details.html?id=${featuredArray[i].id}">More Info</a></div>
+        <div class="featured_card_info">
+          <h2 class="featured_card_title">${featuredArray[i].title.rendered}</h2>
+          <p class="black_p">${featuredArray[i].excerpt.rendered}</p>
+        </div>
+        <div class="more_button"><a href="/html/details.html?id=${featuredArray[i].id}" class="black_p">More Info</a></div>
       </div>`;
     }
   }
 }
-
-// document.querySelector('.index_featured').style.backgroundImage = `
-//     url('${featuredArray._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url}')`;
-
-
-// const gameCards = (GamesArray) => {
-// 	const mainElm = document.querySelector('.featuredInfo');
-//   for (let i = 0; i < GamesArray.length; i++) {
-//     if ((GamesArray[i].categories[0].name === 'Featured') || (GamesArray[i].categories[1].name === 'Featured')) {
-//       mainElm.innerHTML += 
-//       `
-//       <div class="gameCard">
-//       <div class="gameImg"><img src="${GamesArray[i].images[0].src}"/></div>
-//       <h3>${GamesArray[i].name}</h3>
-//       <div class="info-button"><a href="/html/details.html?id=${GamesArray[i].id}">More Info</a></div>
-//       </div>
-//       `;
-//     }
-//   }
-// }
-
-// IMAGES
-
-// let blogAPI2 = [];
-
-// const getMedia = async () => {
-// 	try {
-// 		const response = await fetch(
-// 			'https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/media?per_page=100'
-// 		);
-
-// 		const mediaResponse = await response.json();
-// 		blogAPI2 = mediaResponse;
-//     console.log(blogAPI2)
-
-//     mediaCards(blogAPI2);
-
-// 	} catch (error) {
-// 		// document.querySelector('.alert').innerHTML = showAlertToUser (
-//     //   'An error occured',
-//     //   'danger'
-//     // );
-// 		console.log(error);
-// 	} finally {
-// 		// setTimeout(function () {
-//     //   document.querySelector('.alert').innerHTML = ``;
-//     // }, 5000)
-// 	}
-// };
-
-// getMedia();
-
-
-// const mediaCards = (featuredArray2) => {
-// 	const mainElm = document.querySelector('.featured_container');
-//   for (let i = 0; i < featuredArray2.length; i++) {
-//     if(featuredArray2[i].alt_text === 'Edinburgh'){
-//       mainElm.innerHTML += `
-//     <div class="card__containerBlog2">
-//       <img class="featuredImg" src="${featuredArray2[i].guid.rendered}"/>
-//       `;
-//     }
-//   }
-// };
-
-
-// const getMedia = async () => {
-// 	try {
-// 		const response = await fetch(
-// 			'https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/media?parent='+id
-// 		);
-
-// 		const mediaResponse = await response.json();
-// 		blogAPI2 = mediaResponse;
-//     console.log(blogAPI2)
-
-//     mediaCards(blogAPI2);
-
-// 	} catch (error) {
-// 		// document.querySelector('.alert').innerHTML = showAlertToUser (
-//     //   'An error occured',
-//     //   'danger'
-//     // );
-// 		console.log(error);
-// 	} finally {
-// 		// setTimeout(function () {
-//     //   document.querySelector('.alert').innerHTML = ``;
-//     // }, 5000)
-// 	}
-// };
-
-// getMedia();
-
-
-// const mediaCards = (featuredArray2) => {
-// 	const mainElm = document.querySelector('.featured_container');
-//   for (let i = 0; i < featuredArray2.length; i++) {
-//     if(featuredArray2[i].alt_text === 'Edinburgh'){
-//       mainElm.innerHTML += `
-//     <div class="card__containerBlog2">
-//       <img class="featuredImg" src="${featuredArray2[i].guid.rendered}"/>
-//       `;
-//     }
-//   }
-// };
