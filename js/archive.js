f3a97523cb9114cbf93d1260da11fbd3ddd9f84a
Emma <69841693+xentraz@users.noreply.github.com>
2021-05-30 03:37:18 +0200

    buttons update

--- js/archive.js
+++ js/archive.js
@@ -5,7 +5,7 @@ document.querySelector('.loading').innerHTML =`<img src="https://cdn.dribbble.co
 const getFeatured = async () => {
 	try {
 		const response = await fetch(
-			'https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/posts'
+			'https://noroffcors.herokuapp.com/https://xentraz.tech/wp-json/wp/v2/posts?per_page=100'
 		);
 
 		const featuredResponse = await response.json();
@@ -13,6 +13,12 @@ const getFeatured = async () => {
     console.log(blogAPI);
 
     recentCards(blogAPI);
+    
+    const loadAll = document.querySelector('.load_all'); 
+    loadAll.addEventListener("click", function(){
+      document.querySelector('.recent_posts').innerHTML = '';
+      recentCardsAll(blogAPI);
+      });
 
     const searchText = document.querySelector('#search');
 
@@ -56,6 +62,9 @@ getFeatured();
 const recentCards = (recentArray) => {
   const recentElm = document.querySelector('.recent_posts'); 
   for (let j = 0; j <recentArray.length; j++) {
+    if (j === 10) {
+      break;
+    }
     recentElm.innerHTML += 
     `
     <div class="recent_card">  
@@ -69,6 +78,28 @@ const recentCards = (recentArray) => {
       <img class="recent_img2" src="${recentArray[j].jetpack_featured_media_url}"/>
       <div class="recent_post_link">
       <p class="white_p more_link" onclick="window.location.href='../details.html?id=${recentArray[j].id}'">Read More <i class="fas fa-arrow-right"></i></p>
+      </div>
+    </div>
+    `;
+  }
+}
+
+const recentCardsAll = (recentArray2) => {
+  const recentElm2 = document.querySelector('.recent_posts'); 
+  for (let k = 0; k < recentArray2.length; k++) {
+    recentElm2.innerHTML += 
+    `
+    <div class="recent_card">  
+      <div class="recent_bkg"></div>
+      <h3 class="recent_card_title">${recentArray2[k].title.rendered}</h3>
+      <div class="white_p date">${recentArray2[k].excerpt.rendered}</div>
+      <p class="white_p recent_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, omnis fugit! Fuga nihil odit sed ullam, quod dolor, iste sunt perspiciatis voluptatibus libero enim sit rerum aliquid repellendus accusantium, reiciendis cumque. Voluptatem, nam possimus in provident assumenda architecto adipisci iusto?</p>
+      <div class="recent_img1">
+      <img src="${recentArray2[k].jetpack_featured_media_url}"/>
+      </div>
+      <img class="recent_img2" src="${recentArray2[k].jetpack_featured_media_url}"/>
+      <div class="recent_post_link">
+      <p class="white_p more_link" onclick="window.location.href='../details.html?id=${recentArray2[k].id}'">Read More <i class="fas fa-arrow-right"></i></p>
       </div>
     </div>
     `;
